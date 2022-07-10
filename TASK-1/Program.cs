fda46c91a0e71a7f793f56741555aa0e54f1bce7
Vlad <vlad.melnik@bk.ru>
2022-07-10 15:03:25 +0300

    1 task added

---
+++ TASK-1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int[] CreateRandomArray(int size) 
+{
+      int[] newArray = new int[size]; 
+      for(int i = 0; i < size; i++) 
+      newArray[i] = new Random().Next(100, 999); 
+      return newArray; 
+}
+
+int Evenelements(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+
+    if(array[i]%2 == 0) 
+    sum++;
+
+    return sum;
+}
+void ShowArray(int[] array) 
+{ 
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " "); 
+ 
+    Console.WriteLine(); 
+} 
+ 
+Console.Write("Input size of array: "); 
+int size = Convert.ToInt32(Console.ReadLine()); 
+int[] array = CreateRandomArray(size);
+ShowArray(array);
+int Even = Evenelements(array);
+Console.WriteLine("Sum of even numbers in current array is " + Even);
+
+ 
